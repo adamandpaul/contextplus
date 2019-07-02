7dd67e86654db96867726de82bbe2865214be9a4
Adam Terrey <arterrey@gmail.com>
2019-07-02 16:50:49 +0700

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name='contextplus',
-      version='1.0',
+      version='1.1.dev1',
       description='contextplus',
       long_description=open('README.rst').read(),
       long_description_content_type='text/x-rst',
