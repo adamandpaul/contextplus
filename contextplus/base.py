6dd30e80174368dcd7fe2c2a33d26b02750904a5
Adam Terrey <arterrey@gmail.com>
2019-07-06 19:04:40 +0700

    Re manage method order int he class DomainBase

--- contextplus/base.py
+++ contextplus/base.py
@@ -8,7 +8,6 @@ from .behaviours import BehaviourTinterface
 from .behaviours import BehaviourTraversal
 from .behaviours import BehaviourTraversalPathUtilities
 from .behaviours import BehaviourWorkflow
-from .reify import reify
 from typing import Optional
 
 
@@ -19,7 +18,51 @@ class DomainBase(BehaviourTinterface, BehaviourTraversal, BehaviourWorkflow, Beh
     Designed to apply common functions to all domain objects.
     """
 
+    # == Class Methods ==
+
+    @classmethod
+    def get_meta_title(cls) -> str:
+        """A human readable title of the kind of object"""
+        return cls.__name__
+
+    # == Properties ==
+
+    # Name. As in the name in the traversal parth
+
     _name = None
+
+    @property
+    def name(self):
+        """Return the name unless it is None. In which case throw an AttributeError"""
+        if self._name is None:
+            raise AttributeError('Name not yet set')
+        return self._name
+
+    __name__ = name  # pyramid expects this as the name interface
+
+    parent = None
+
+    @property
+    def __parent__(self):
+        """Pyramid traversal interface"""
+        return self.parent
+
+    @property
+    def title(self) -> str:
+        """A human readable title of this object instance"""
+        meta_title = self.get_meta_title()
+        name = self.name
+        if name is not None:
+            return f'{meta_title}: {self.name}'
+        else:
+            return f'{meta_title}'
+
+    @property
+    def description(self) -> str:
+        """A short description of the this object"""
+        return ''
+
+    # == Methods ==
 
     def __init__(self, parent=None, name: Optional[str] = None):
         """Initalize the DomainBase object
@@ -31,47 +74,6 @@ class DomainBase(BehaviourTinterface, BehaviourTraversal, BehaviourWorkflow, Beh
         self.parent = parent
         self.set_name(name)
 
-    # Name property
-
     def set_name(self, name: str = None):
         """Allow setting the name"""
         self._name = name
-
-    @property
-    def name(self):
-        """Return the name unless it is None. In which case throw an AttributeError"""
-        if self._name is None:
-            raise AttributeError('Name not yet set')
-        return self._name
-
-    @reify
-    def __name__(self):
-        """Pyramid traversal interface"""
-        return self.name
-
-    @reify
-    def __parent__(self):
-        """Pyramid traversal interface"""
-        return self.parent
-
-    # Human friendly titles descriptions
-
-    @classmethod
-    def get_meta_title(cls) -> str:
-        """A human readable title of the kind of object"""
-        return cls.__name__
-
-    @reify
-    def title(self) -> str:
-        """A human readable title of this object instance"""
-        meta_title = self.get_meta_title()
-        name = self.name
-        if name is not None:
-            return f'{meta_title}: {self.name}'
-        else:
-            return f'{meta_title}'
-
-    @reify
-    def description(self) -> str:
-        """A short description of the this object"""
-        return ''
