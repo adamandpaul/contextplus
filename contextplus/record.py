aa5ae40bf871473d09444eed697a302c2dc57fb0
Peter Schmidt <peter@peterjs.com>
2019-07-10 18:12:13 +1000

    fix: Resolve dict is a default mutable argument warning
    Note: Does not resolve the 'id' shadows the built in function id warning, no need to borrow from Django and use pk as the record could be a nonrel data store.

--- contextplus/record.py
+++ contextplus/record.py
@@ -18,7 +18,7 @@ class DomainRecord(base.DomainBase):
         self._record = record
 
     @classmethod
-    def from_id(cls, parent=None, name: str = None, id={}) -> 'DomainRecord':
+    def from_id(cls, parent=None, name: str = None, id: dict = None) -> 'DomainRecord':
         """Pull a record and construct this domain object"""
         raise NotImplementedError()
 
