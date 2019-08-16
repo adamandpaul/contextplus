cdc57e62b4d0479184db8128b9c17891897a4910
Adam Terrey <arterrey@gmail.com>
2019-08-16 11:33:06 +0700

    Add traversal tests

---
+++ contextplus/behaviour/traversal_test.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+from . import traversal
+from unittest import TestCase
+
+
+class TestTraversal(TestCase):
+    def setUp(self):
+
+        self.root = traversal.TraversalBehaviour()
+        self.root.name = "root"
+
+        self.mid = traversal.TraversalBehaviour()
+        self.mid.name = "mid"
+        self.mid.parent = self.root
+
+        self.leaf = traversal.TraversalBehaviour()
+        self.leaf.name = "leaf"
+        self.leaf.parent = self.mid
+
+    def test_getitem(self):
+        result = self.root.get("blah", "notfound")
+        self.assertEqual(result, "notfound")
+
+        with self.assertRaises(KeyError):
+            self.root["blah"]
+
+    def test_iter_ancestors(self):
+        result = [a.name for a in self.leaf.iter_ancestors()]
+        self.assertEqual(result, ["mid", "root"])
+
+    def test_path_names(self):
+        result = self.leaf.path_names
+        self.assertEqual(result, ("root", "mid", "leaf"))
+
+    def test_root(self):
+        result = self.leaf.root
+        self.assertEqual(result, self.root)
